6505a1c4fada50d76bd1eada8da3a94125e16476
unknown <sbendiola@.US.DRWHoldings.com>
2009-07-10 09:44:36 -0500

    added lamda expression example

--- csharp/Sandbox/Linq/ExpressionTest.cs
+++ csharp/Sandbox/Linq/ExpressionTest.cs
@@ -1,11 +1,66 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
+using System.Linq.Expressions;
+using NUnit.Framework;
 
 namespace Sandbox.Linq
 {
-    class ExpressionTest
+    [TestFixture]
+    public class ExpressionTest
     {
+        [Test]
+        public void ShouldBeMemberExpression()
+        {
+            Expression<Func<Foo, int>> expression = x => x.IntProperty;
+            LambdaExpression lambda = expression;
+            Assert.AreEqual(1, lambda.Parameters.Count);
+            var first = lambda.Parameters.First();
+            Assert.AreEqual("x", first.Name);
+            Assert.AreEqual(ExpressionType.Parameter, first.NodeType);
+            Assert.AreEqual(typeof(Foo), first.Type);
+            var memberExpression = (MemberExpression)lambda.Body;
+            Assert.IsNotNull(memberExpression);
+        }
+
+        [Test]
+        public void ShouldBeMethodCallExpression()
+        {
+            Expression<Func<Foo, int>> expression = x => x.Bar.IntMethod();
+            LambdaExpression lambda = expression;
+            Assert.AreEqual(1, lambda.Parameters.Count);
+            var first = lambda.Parameters.First();
+            Assert.AreEqual("x", first.Name);
+            Assert.AreEqual(ExpressionType.Parameter, first.NodeType);
+            Assert.AreEqual(typeof(Foo), first.Type);
+            var methodCall = lambda.Body as MethodCallExpression;
+            Assert.IsNotNull(methodCall);
+            Assert.AreEqual(0, methodCall.Arguments.Count);
+            Assert.AreEqual("IntMethod", methodCall.Method.Name);
+            var actual = methodCall.Object;
+            var memberExpression = actual as MemberExpression;
+            Assert.IsNotNull(memberExpression);
+            Assert.AreEqual("Bar", memberExpression.Member.Name);
+            
+        }
+
+        public class Foo
+        {
+            public int IntProperty { get; set; }
+            public int IntMethod()
+            {
+                return 0;
+            }
+
+            public Bar Bar { get; set;}
+        }
+
+        public class Bar
+        {
+            public int IntProperty { get; set; }
+            public int IntMethod()
+            {
+                return 0;
+            }
+        }
     }
 }
